a5c8dd014d4904c9bee10082fb52db1780c14b50
Lidiaescas <lidia.castellanos15@gmail.com>
2026-04-09 13:29:02 +0200

    feat: nuevos cambios

--- app/src/main/java/com/example/qtengo/ui/screens/DishScreen.kt
+++ app/src/main/java/com/example/qtengo/ui/screens/DishScreen.kt
@@ -17,7 +17,6 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.qtengo.data.model.Dish
 
 @Composable
 fun DishScreen(
